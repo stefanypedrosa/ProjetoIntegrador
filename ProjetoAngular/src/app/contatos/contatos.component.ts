b20fe2281974fac1bfa5c962db8da9467fc02241
stefanypedrosa <stefany.pedrosa@fatec.sp.gov.br>
2019-12-20 09:16:05 -0300

    ajustes

--- ProjetoAngular/src/app/contatos/contatos.component.ts
+++ ProjetoAngular/src/app/contatos/contatos.component.ts
@@ -13,7 +13,7 @@ export class ContatosComponent implements OnInit {
   private tel: string;
   private msg: any;
   private filtro: any = /^([a-zA-zà-úÀ-Ú]|\s+)+$/;
-  private num: any = /([0-9])/;
+  private num: any = /^[0-9]+$/;
   private _msgErroN: string = null;
   private _msgErroS: string = null;
   private _msgErroE: string = null;
@@ -28,44 +28,44 @@ export class ContatosComponent implements OnInit {
     if (this.nome == "" || this.sobrenome == "" || this.email == "" || this.tel == null || this.msg == "") {
       alert('Preencha todos os campos');
     }
-    if (!this.filtro.test(this.nome)){
+    if (!this.filtro.test(this.nome)) {
       this.nome = "";
       this._msgErroN = "Dado inválido";
     }
-    else{
+    else {
       this._msgErroN = null;
     }
     if (!this.filtro.test(this.sobrenome)) {
       this.sobrenome = "";
       this._msgErroS = "Dado inválido";
     }
-    else{
+    else {
       this._msgErroS = null;
     }
-    
+
     if (this.email.indexOf("@") == -1 && this.email.indexOf("@") > 1 || this.email.indexOf(".") == -1) {
       this.email = "";
       this._msgErroE = "Dado inválido";
     }
-    else{
+    else {
       this._msgErroE = null;
     }
-    
-    if(!this.num.test(this.tel)){
+
+    if (!this.num.test(this.tel)) {
       this.tel = null;
       this._msgErroT = `Apenas digitos`;
     }
-    else{
+    else {
       this._msgErroT = null;
     }
-    if(this.tel.length < 11){
+    if (this.tel.length < 11) {
       this.tel = null;
       this._msgErroT = `Digite 11 digitos`;
     }
-    else{
+    else {
       this._msgErroT = null;
-    }    
-    if(this.nome != "" && this.sobrenome != "" && this.email != "" && this.tel != null && this.msg != "") {
+    }
+    if (this.nome != "" && this.sobrenome != "" && this.email != "" && this.tel != null && this.msg != "") {
       alert("Dados enviados com SUCESSO!!")
       this.nome = "";
       this.sobrenome = "";
